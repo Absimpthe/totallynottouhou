7c520c880129012d48a0fb82b05021f4b005a9ea
swyannnn <swyannnn@gmail.com>
2024-03-05 13:46:36 +0800

    add smtg for better render in desktopLaucher.java

--- desktop/src/com/tnt/game/DesktopLauncher.java
+++ desktop/src/com/tnt/game/DesktopLauncher.java
@@ -4,15 +4,16 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public satatic void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 
 		// Set the window size
 		config.setWindowedMode(1280, 720);
-
 		// Disable window resizing
 		config.setResizable(false);
 		config.setTitle("Aquamarine");
+		config.useVsync(true);
+		config.setForegroundFPS(60);
 		// Create and start the application
 		new Lwjgl3Application(new aquamarine(), config);
 	}
