1192c0e56c50e96e230ad324788a386ec06d9025
ced <cedro755@gmail.com>
2024-03-05 09:01:08 +0800

    test

---
+++ core/src/com/tnt/game/Level.java
@@ -0,0 +1,39 @@
+package com.tnt.game;
+import com.badlogic.gdx.Screen;
+
+public class Level implements Screen {
+    private totallynottouhou game;
+
+    public Level(totallynottouhou game) {
+        this.game = game;
+
+    }
+    @Override
+    public void show() {
+
+    }
+    @Override
+    public void render(float delta) {
+
+    }
+    @Override
+    public void resize(int width, int height) {
+
+    }
+    @Override
+    public void pause() {
+
+    }
+    @Override
+    public void resume() {
+
+    }
+    @Override
+    public void hide() {
+
+    }
+    @Override
+    public void dispose() {
+
+    }
+}
